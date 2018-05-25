581199c7b27ee4d83c0eb59ec1e4d4a0a5d91aa7
deepgunner <deepindersingh.23@gmail.com>
2018-05-25 12:05:42 +0530

    added word counter logic

--- server.js
+++ server.js
@@ -6,6 +6,38 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+var wordCount = []
+
+
+//Function to get the word count from the file
+function wordCounter(url, callback) {
+    request(url, (err, res, text) => {
+        if (err) throw err
+
+        var wordPool = {}
+        wArray = text.toLowerCase().split(/\W+/)
+    
+        wArray.forEach((key) => {
+            if (wordPool.hasOwnProperty(key)) {
+                wordPool[key] += 1
+            } else {
+                wordPool[key] = 1
+            }
+        })
+    
+        wCount = Object.keys(wordPool).map((key) => {
+            return {
+                word: key,
+                count: wordPool[key]
+            }
+        })
+    
+        wCount.sort((first,second) => second.count - first.count)
+
+        return callback(wordCount)
+    })
+}
+
 //API get request
 app.get('/data', (req, res) => {
     wordCounter('http://terriblytinytales.com/test.txt', (data) => {
